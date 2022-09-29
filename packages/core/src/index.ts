85e23dbed3815f922e43ad0a17e4545cc312b097
awar3ness <207484662@qq.com>
2022-09-29 17:28:09 +0800

    refactor chore: removed useless things. added some events types.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,73 @@
 import { Context } from "cordis"
-import { GameState } from "./regulates/interfaces"
+import { Player } from './player'
+import * as lifecycle from './lifecycle'
+import { Deck, GameStage, GameStep } from "./regulates/type"
+import { type } from "os"
+import { Slot, SlotList } from "./regulates/interfaces"
+export interface GameState {
+  playerState: Player[],
+  process: {
+    stage: GameStage,
+    step: GameStep,
+    /* 0: Alice, 1: Bob */
+    priority: number,
+    turn: number,
+    round: number,
+  }
+}
+declare module 'cordis' {
+  
+  export interface Context {
+    duel: GameState
+  }
 
+  export interface Events {
+    //game event
+    'game-start': () => void
+    'game-end': () => void
+
+    //turn event
+    'turn-instant': (id: number) => void
+    'turn-main': (id: number) => void
+    'turn-drop': () => void
+
+    //player event
+    'player-play': (id: number) => void
+    'player-masteryup': (id:number) => void
+    'player-masterydown': (id:number) => void
+    'player-getcard': (id:number, fool:boolean) => void
+    
+    //card event
+    'card-AH': (id: number, pos: SlotList) => void
+    'card-AP': (id: number, pos: SlotList) => void
+    'card-AN': (id: number, pos: SlotList) => void
+    'card-2': (id: number, pos: SlotList, opt: boolean) => void
+    'card-3': (id: number, pos: SlotList) => void
+    'card-4': (id: number, pos: SlotList) => void
+    'card-5': (id: number, cardset: number[]) => void
+    'card-6': (id: number) => void
+    'card-7': (id: number) => void
+    'card-8': (id: number, pos: SlotList) => void
+    'card-9': (id: number, pos: SlotList) => void
+    'card-10': (id: number, pos: SlotList) => void
+    'card-J': (id: number, pos: SlotList) => void
+    'card-Q': (id: number, pos: SlotList) => void
+    'card-K': (id: number) => void
+    'card-BJ': (id: number) => void
+    'card-RJ': (id: number) => void
+    'card-0': (id: number) => void
+  }
+  
+}
 
 export function duelInit(app: Context) {
-  app.on('duel-start', () => {
-    // app.duel.playerState = [new Player(app, {initialMastery: 10}), new Player(app, {initialMastery: 10})];
+  /*app.on('duel-start', (decks: Deck[]) => {
+    app.duel.playerState = [new Player(app, {id: 0, deck: decks[0]}), new Player(app, {id: 1, deck: decks[1]})];
   });
+
+  app.emit('duel-start', []);*/
+
+
   
   // app.socket.on('message', (msg) => {
   //   ctx.emit('session', {
--- packages/core/src/regulates/interfaces.ts
+++ packages/core/src/regulates/interfaces.ts
@@ -16,17 +16,5 @@ export type Card = string;
 
 export type Position = [number, number, number];
 
-export interface GameState {
-  board: Board,
-  player: Player[],
-  global: {
-    round: number,
-    turn: number,
-    stage: GameStage, 
-    result: Record<string, number> // 每名玩家的名次
-  }
-}
-
-
 // 服务端发送给客户端的格子集合
 export type SlotList = Array<[number, number, number]>; // t, x, y
