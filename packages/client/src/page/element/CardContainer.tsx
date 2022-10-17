10dd7b7b4048a619dc031e0c777ea9ffb9d42c6c
FlashLizard <1256854232@qq.com>
2022-10-17 22:23:04 +0800

    feat: add CardTip(you can see it when you move your mouse on a card) improve the view style

--- packages/client/src/page/element/CardContainer.tsx
+++ packages/client/src/page/element/CardContainer.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Group, Rect, Tag, Text } from 'react-konva';
+import { Group, Rect, Tag, Text, Label } from 'react-konva';
+
+class Card {
+    public name: string;
+    public introduce: string;
+    public color: string;
+    constructor(name: string, introduce: string, color: string) {
+        this.color = color;
+        this.name = name;
+        this.introduce = introduce;
+    }
+}
 
 const cardMap: {
-    [key: string]: string
+    [key: string]: Card
 } = {
-    ["None"]: "red",
-    ["test1"]: "black",
-    ["test2"]: "blue",
-    ["test3"]: "green",
+    ["None"]: new Card("None", "none", "red"),
+    ["test1"]: new Card("test1", "none", "black"),
+    ["test2"]: new Card("test2", "none", "blue"),
+    ["test3"]: new Card("test3", "none", "green"),
 }
 
 type CardContainerProps = typeof CardContainer.defaultProps & {
@@ -19,13 +30,14 @@ type CardContainerProps = typeof CardContainer.defaultProps & {
 
 interface CardContainerState {
     cardList?: string[]
+    tipCard?: number
 }
 
 class CardContainer extends React.Component<CardContainerProps, CardContainerState> {
 
     static defaultProps = {
-        x: window.innerWidth/2,
-        y: window.innerHeight*7/8,
+        x: window.innerWidth / 2,
+        y: window.innerHeight * 7 / 8,
         cardWidth: 100,
         cardList: ["None", "test1", "test2", "test3"]
     }
@@ -33,33 +45,78 @@ class CardContainer extends React.Component<CardContainerProps, CardContainerSta
     constructor(props) {
         super(props)
         this.state = {
-            cardList: this.props.cardList
+            cardList: this.props.cardList,
+            tipCard: null
         }
     }
 
     render(): React.ReactNode {
         let theta = 30;
-        let mid = (this.state.cardList.length-1)/2;
+        let mid = (this.state.cardList.length - 1) / 2;
         let cards = this.state.cardList.map((value, index) => {
             return (
-            <Rect
-            width={this.props.cardWidth}
-            height={this.props.cardWidth*1.4}
-            offsetX={this.props.cardWidth/2}
-            offsetY={this.props.cardWidth*1.2}
-            fill={cardMap[value]}
-            rotation={theta*(index-mid)}
-            ></Rect>)
+                <Rect
+                    onMouseEnter={() => { console.log(index); this.setState({ tipCard: index }) }}
+                    width={this.props.cardWidth}
+                    height={this.props.cardWidth * 1.4}
+                    offsetX={this.props.cardWidth / 2}
+                    offsetY={this.props.cardWidth * 1.2}
+                    fill={cardMap[value].color}
+                    rotation={theta * (index - mid)}
+                ></Rect>)
         })
 
-        console.log(cards)
         return (
             <Group
-            x={this.props.x}
-            y={this.props.y}
+                onMouseLeave={() => this.setState({ tipCard: null })}
+                x={this.props.x}
+                y={this.props.y}
             >
+
                 {cards}
+                {this.state.tipCard != null ? this.renderTip(this.state.tipCard) : <Text></Text>}
             </Group>
+        )
+    }
+
+    renderTip(index: number): React.ReactNode {
+        let mid = (this.state.cardList.length - 1) / 2;
+        let theta = (90 - 30 * (index - mid)) / 180 * Math.PI
+        let card = cardMap[this.state.cardList[index]];
+        return (
+
+            <Label
+                x={this.props.cardWidth * 1.5 * Math.cos(theta)}
+                y={-this.props.cardWidth * 1.5 * Math.sin(theta)}
+            >
+                <Tag
+                    fill='grey'
+                    pointerDirection="down"
+                    pointerHeight={10}
+                    pointerWidth={10}
+                    lineJoin='round'
+                    shadowColor='grey'
+                    shadowBlur={10}
+                    shadowOffsetX={10}
+                    shadowOffsetY={10}
+                    shadowOpacity={0.5}
+                    cornerRadius={10}
+                >
+                </Tag>
+                <Text
+                    text={`名称: ${card.name}\n\n描述: ${card.introduce}`}
+                    fill='white'
+                    fontSize={20}
+                    padding={7}
+                ></Text>
+            </Label>
+
+            // <Text
+            // text={`名称: ${card.name}\n\n描述: ${card.introduce}`}
+            // x={this.props.cardWidth*3*Math.cos(theta)}
+            // y={-this.props.cardWidth*3*Math.sin(theta)}
+            // fontSize={30}
+            // ></Text>
         )
     }
 }
--- packages/client/src/page/element/PlayerList.tsx
+++ packages/client/src/page/element/PlayerList.tsx
@@ -1,28 +1,61 @@
 import React from 'react';
 import { Player } from '../../regulates/Interfaces';
-import { Group, Text } from 'react-konva';
+import { Group, Text, Label, Tag } from 'react-konva';
 
 interface PlayerListProps {
     playList?: Player[]
 }
 
 class PlayerList extends React.Component<PlayerListProps> {
+
+    static colorList = [
+        "#4169E1",
+        "#00FA9A",
+        "#ADFF2F",
+        "#FFA500",
+        "#FF4500",
+    ]
+
     constructor(props) {
         super(props)
 
     }
-    
+
     render(): React.ReactNode {
-        let players = this.props.playList.map((val, index)=>{
+        let players = this.props.playList.map((val, index) => {
             return (
-            <Text text={
-                `玩家${index+1}:   ` + 
-                (val.alive?"存活":"死亡") +
-              `  智识: ${val.mastery}` +
-              `  位置: 第${val.position[0]+1}层 ${val.position[1]}, ${val.position[2]}`}
-             fontSize={20}
-             y={30*index}
-             ></Text>)
+
+                <Label
+                    y={50 * index}
+                >
+                    <Tag
+                        fill={index < PlayerList.colorList.length ? PlayerList.colorList[index]:'grey'}
+                        lineJoin='round'
+                        shadowColor='grey'
+                        shadowBlur={10}
+                        shadowOffsetX={5}
+                        shadowOffsetY={5}
+                        shadowOpacity={0.5}
+                    >
+                    </Tag>
+                    <Text text={
+                        `玩家${index + 1}:   ` +
+                        (val.alive ? "存活" : "死亡") +
+                        `  智识: ${val.mastery}` +
+                        `  位置: 第${val.position[0] + 1}层 ${val.position[1]}, ${val.position[2]}`}
+                        fontSize={20}
+                        padding={7}
+                    ></Text>
+                </Label>)
+
+            // <Text text={
+            //     `玩家${index+1}:   ` + 
+            //     (val.alive?"存活":"死亡") +
+            //   `  智识: ${val.mastery}` +
+            //   `  位置: 第${val.position[0]+1}层 ${val.position[1]}, ${val.position[2]}`}
+            //  fontSize={20}
+            //  y={30*index}
+            //  ></Text>)
         })
         console.log(this.props.playList)
         console.log(players)
