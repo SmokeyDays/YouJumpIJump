89e59837af138bd9de9f7e0da1b57199ef1a9883
FlashLizard <1256854232@qq.com>
2022-11-08 17:49:26 +0800

    添加音乐test

--- packages/client/src/page/GamePage.tsx
+++ packages/client/src/page/GamePage.tsx
@@ -10,6 +10,7 @@ import { socket } from "../communication/connection";
 import { Card, CardPara, Position, RequestSignal, SignalPara } from "../../../core/src/regulates/interfaces";
 import { BlockLike } from "typescript";
 import { Player } from "../../../core/src/player";
+//import Bgm from "../assets/musics/bgm.flac"
 
 
 export let LocalPlayer = null
@@ -42,9 +43,9 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
     GamePage.instance = this
     this.state = {
       boards: [
-        new BoardInfo(2 * (this.props.gameState.player.length - 1) +4, 'white', 'green', <Slot color='MediumAquamarine'></Slot>),
-        new BoardInfo(2 * (this.props.gameState.player.length - 1) +3, 'white', 'green', <Slot color='LightSeaGreen'></Slot>),
-        new BoardInfo(2 * (this.props.gameState.player.length - 1) +2, 'white', 'green', <Slot color='DarkCyan'></Slot>),
+        new BoardInfo(2 * (this.props.gameState.player.length - 1) + 4, 'white', 'green', <Slot color='MediumAquamarine'></Slot>),
+        new BoardInfo(2 * (this.props.gameState.player.length - 1) + 3, 'white', 'green', <Slot color='LightSeaGreen'></Slot>),
+        new BoardInfo(2 * (this.props.gameState.player.length - 1) + 2, 'white', 'green', <Slot color='DarkCyan'></Slot>),
       ],
       showingCard: "",
       currentBoard: 2,
@@ -57,26 +58,26 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
     };
 
     //this.state.gameState.global.turn=-1
-        
+
     LocalPlayer = this.props.localPlayer;
 
     socket.on('renew-game-state', (val: { state: GameState, localPlayer: number }) => {
-      console.log('!!!renew-game-state',val);
+      console.log('!!!renew-game-state', val);
       this.loadGameState(val.state)
       this.setState({ gameState: val.state })
     })
     socket.on('request-signal', (val: SignalPara) => {
-      console.log('!!!request-signal',val);
+      console.log('!!!request-signal', val);
       switch (val.type) {
         case 'recast':
-          this.setState({isInRecast: true})
+          this.setState({ isInRecast: true })
           break;
         case 'card':
-          if (val.stage=='main') {
-            this.setState({stage:1})
+          if (val.stage == 'main') {
+            this.setState({ stage: 1 })
           }
           else {
-            this.setState({stage:0})
+            this.setState({ stage: 0 })
           }
           break;
         case 'action':
@@ -84,11 +85,11 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
           break;
       }
     })
-    
-  socket.on('return-pos-set', (val: Position[]) => {
-    console.log('!!!return-pos-set',val);
-    this.setFreSlotList(val)
-  })
+
+    socket.on('return-pos-set', (val: Position[]) => {
+      console.log('!!!return-pos-set', val);
+      this.setFreSlotList(val)
+    })
     this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
@@ -96,6 +97,12 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
     // console.log(this.props.gameState);
     return (
       <div>
+        {/* <audio
+          autoPlay={true}
+          loop={true}
+          preload="auto"
+          src="../music/纯音乐-忧伤还是快乐.mp3">
+        </audio> */}
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <GameCanvas
             freSlotList={this.state.freSlotList[this.state.currentBoard]}
@@ -108,7 +115,7 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
               (v, i) => v.alive && v.position[0] === this.state.currentBoard && v)}
           ></GameCanvas>
           <UI
-          
+
             isInRecast={this.state.isInRecast}
             stage={this.state.stage}
             currentBoard={this.state.currentBoard}
@@ -128,25 +135,25 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
     );
   }
 
-  isPlayerEqual(id:number, player1: Player, player2: Player): boolean {
-    if(player1.alive!=player2.alive) {
-      PubSub.publish('alert-pubsub-message',{str:`${player1.name}被淘汰了!`,dur:3})
+  isPlayerEqual(id: number, player1: Player, player2: Player): boolean {
+    if (player1.alive != player2.alive) {
+      PubSub.publish('alert-pubsub-message', { str: `${player1.name}被淘汰了!`, dur: 3 })
     }
-    if(player2.magician && !player1.magician) {
-      PubSub.publish('alert-pubsub-message',{str:`${player1.name}获得了悬浮状态`,dur:3})
+    if (player2.magician && !player1.magician) {
+      PubSub.publish('alert-pubsub-message', { str: `${player1.name}获得了悬浮状态`, dur: 3 })
     }
     return true;
   }
 
   loadGameState(state: GameState) {
-      let boards = state.board
-      console.log("Boardnew",boards)
-      for( let index in boards) {
-        let pos = index.split(',')
-        let i = Number(pos[0]),j=Number(pos[1]),k=Number(pos[2]);
-        this.state.boards[i].setSlotStatus(j,k,boards[index].isBursted);
-      }
-      CardContainer.instance.setCard(state.player[LocalPlayer].hand);
+    let boards = state.board
+    console.log("Boardnew", boards)
+    for (let index in boards) {
+      let pos = index.split(',')
+      let i = Number(pos[0]), j = Number(pos[1]), k = Number(pos[2]);
+      this.state.boards[i].setSlotStatus(j, k, boards[index].isBursted);
+    }
+    CardContainer.instance.setCard(state.player[LocalPlayer].hand);
 
   }
 
@@ -204,10 +211,10 @@ class GamePage extends React.Component<GamePageProps, GamePageState> {
   }
 
   setInRecast(val: boolean): void {
-    this.setState({isInRecast: val})
+    this.setState({ isInRecast: val })
   }
 
-  
+
 }
 
 export default GamePage;
