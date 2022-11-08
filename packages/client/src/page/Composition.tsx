cc3341f28f8f8a05742e95573c1ce51f9fece83f
hotwords123 <hotwords123@gmail.com>
2022-11-08 21:56:06 +0800

    feat: better toast

--- packages/client/src/page/Composition.tsx
+++ packages/client/src/page/Composition.tsx
@@ -3,12 +3,13 @@ import './Composition.css';
 
 interface AlertWindowProps {
   message: string,
+  shown: boolean,
 }
 
 export class AlertWindow extends Component<AlertWindowProps, {}> {
   render(): React.ReactNode {
     return (
-      <div className='alert-message-window'>
+      <div className={`alert-message-window ${this.props.shown ? "shown" : "hidden"}`}>
         <div className='alert-message-content'>
           {this.props.message}
         </div>
