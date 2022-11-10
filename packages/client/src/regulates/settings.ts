7babe5434282aae66e8887cdeafa5304eb409134
awar3ness <207484662@qq.com>
2022-11-10 20:36:18 +0800

    fixed: spy drawcard

--- packages/client/src/regulates/settings.ts
+++ packages/client/src/regulates/settings.ts
@@ -1,5 +1,5 @@
 // Setup the ServerURL.
 // Remember that don't use "https" instead of "http", or it may cause ERR_SSL_PROTOCOL_ERROR.
-export const serverURL = "http://localhost:1234";
+export const serverURL = "http://144.34.219.61:1234";
 
 export const DEBUG_MODE = true;
